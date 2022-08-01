594f539c8ef8de43e0c63dd59c7aa960f849a95b
Никита <nik.findyurov@mail.ru>
2022-08-02 02:30:09 +0700

    new commit

---
+++ Vector/src/acade/findyrov/vector/Main.java
@@ -0,0 +1,13 @@
+package acade.findyrov.vector;
+
+import java.util.Vector;
+
+public class Main {
+    public static void main(String[] args) {
+        Vector vector = new Vector<>();
+
+        vector.add(10);
+
+        System.out.println(vector);
+    }
+}
---
+++ Vector/src/acade/findyrov/vector/VectorClass.java
@@ -0,0 +1,7 @@
+package acade.findyrov.vector;
+
+import java.util.Vector;
+
+public class VectorClass {
+
+}
