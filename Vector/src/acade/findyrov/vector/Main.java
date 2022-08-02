ff47296991a5a28b475c6aa0fdc7d8813e058f89
Никита <nik.findyurov@mail.ru>
2022-08-02 12:10:09 +0700

    new commit

--- Vector/src/acade/findyrov/vector/Main.java
+++ Vector/src/acade/findyrov/vector/Main.java
@@ -1,13 +1,35 @@
 package acade.findyrov.vector;
 
+import java.util.Arrays;
 import java.util.Vector;
 
 public class Main {
     public static void main(String[] args) {
-        Vector vector = new Vector<>();
+        Vector<Object> vector1 = new Vector<>(5);
+        vector1.add(5);
 
-        vector.add(10);
+        Vector<Object> vector2 = new Vector<>(vector1);
+        System.out.println(vector1);
+        System.out.println(vector2);
 
-        System.out.println(vector);
+        String[] mas = {"Hello", "Bye", "ok"};
+        Vector<String> vector3 = new Vector<>(Arrays.asList(mas));
+
+        System.out.println(vector3);
+
+        Vector<String> vector4 = new Vector<>();
+        int n = 5;
+        for (int i = 0; i < n; i++) {
+            if (i >= mas.length) {
+                vector4.add(null);
+            } else {
+                vector4.addElement(mas[i]);
+            }
+        }
+        System.out.println(vector4);
+
+        VectorClass newvector = new VectorClass(5, 6.5, 6, 7.9);
+
+        System.out.println(newvector.getSizeee());
     }
 }
--- Vector/src/acade/findyrov/vector/VectorClass.java
+++ Vector/src/acade/findyrov/vector/VectorClass.java
@@ -3,5 +3,18 @@ package acade.findyrov.vector;
 import java.util.Vector;
 
 public class VectorClass {
+    private int n;
+    private Vector vector;
 
+    public VectorClass(int n, double n1, double n2, double n3) {
+        Vector<Double> vector = new Vector<>(n);
+        this.vector = vector;
+        vector.add(n1);
+        vector.add(n2);
+        vector.add(n3);
+    }
+
+    public double getSizeee() {
+        return vector.size();
+    }
 }
